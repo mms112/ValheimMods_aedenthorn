a7e9fb9cce990a627db9f3f81644c72d4cb9eae4
mms112 <mms112@users.noreply.github.com>
2025-07-26 20:54:59 +0200

    Reworked BuildingRepairRequiresMats mod. Old version did not work at all.

--- BuildingRepairRequiresMats/BepInExPlugin.cs
+++ BuildingRepairRequiresMats/BepInExPlugin.cs
@@ -16,37 +16,45 @@ namespace BuildingRepairRequiresMats
 
         public static ConfigEntry<bool> modEnabled;
         public static ConfigEntry<bool> isDebug;
-        public static ConfigEntry<bool> requireMats;
-        public static ConfigEntry<bool> leftClick;
         public static ConfigEntry<float> materialRequirementMult;
-        public static ConfigEntry<string> modKey;
-        public static ConfigEntry<string> titleTooltipColor;
-        public static ConfigEntry<string> freeTooltipString;
-        public static ConfigEntry<string> hasEnoughExternalTooltipString;
-        public static ConfigEntry<string> hasEnoughTooltipString;
-        public static ConfigEntry<string> notEnoughTooltipString;
-        public static ConfigEntry<int> nexusID;
-        public static List<Container> containerList = new List<Container>();
+        public static ConfigEntry<float> enemyBuildRange;
+        public static ConfigEntry<string> enemyBuildException;
 
         public static BepInExPlugin context;
-        public static Assembly epicLootAssembly;
 
         public static void Dbgl(string str = "", bool pref = true)
         {
             if (isDebug.Value)
                 Debug.Log((pref ? typeof(BepInExPlugin).Namespace + " " : "") + str);
         }
+
+        private static bool HaveEnemyInRange(Player me, Vector3 point, float range)
+        {
+            foreach (Character allCharacter in Character.GetAllCharacters())
+            {
+                MonsterAI monster = allCharacter.GetComponent<MonsterAI>();
+                if (monster != null)
+                {
+                    if (!monster.m_avoidLand && BaseAI.IsEnemy(me, allCharacter) &&
+                        (Vector3.Distance(allCharacter.transform.position, point) < range) &&
+                        (me.IsTargeted() || me.IsSensed() || monster.IsAlerted()))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
         public void Awake()
         {
             context = this;
             modEnabled = Config.Bind<bool>("General", "Enabled", true, "Enable this mod");
             isDebug = Config.Bind<bool>("General", "IsDebug", true, "Enable debug logs");
-            //nexusID = Config.Bind<int>("General", "NexusID", 1011, "Nexus mod ID for updates");
-            freeTooltipString = Config.Bind<string>("General", "FreeTooltipString", "<color=#00FF00FF>Repair: Free</color>", "String in tooltip for items that don't need resources to repair.");
-            hasEnoughTooltipString = Config.Bind<string>("General", "HasEnoughTooltipString", "<color=#00FF00FF>Repair: {0}</color>", "String in tooltip for items with enough resources to repair. {0} is replaced by the amounts needed.");
-            hasEnoughExternalTooltipString = Config.Bind<string>("General", "HasEnoughExternalTooltipString", "<color=#FFFF00FF>Repair: {0}</color>", "String in tooltip for items with enough resources to repair from mods. {0} is replaced by the amounts needed.");
-            notEnoughTooltipString = Config.Bind<string>("General", "NotEnoughTooltipString", "<color=#FF0000FF>Repair: {0}</color>", "String in tooltip for items with not enough resources to repair. {0} is replaced by the amounts needed.");
             materialRequirementMult = Config.Bind<float>("General", "MaterialRequirementMult", 0.5f, "Multiplier for amount of each material required.");
+            enemyBuildRange = Config.Bind<float>("General", "EnemyBuildRange", 20.0f, "Minimum distance to the nearest enemy to allow building/repairing.");
+            
 
             if (!modEnabled.Value)
                 return;
@@ -54,197 +62,143 @@ namespace BuildingRepairRequiresMats
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
 
         }
-        public void Start()
+
+        [HarmonyPatch(typeof(Player), nameof(Player.Repair))]
+        static class Player_Repair_Patch
         {
-            if(Chainloader.PluginInfos.ContainsKey("randyknapp.mods.epicloot"))
-                epicLootAssembly = Chainloader.PluginInfos["randyknapp.mods.epicloot"].Instance.GetType().Assembly;
-
-        }
-
-        public static void RepairClickedItem(InventoryGrid grid, UIInputHandler element, Inventory inventory)
-        {
-            Vector2i buttonPos = Traverse.Create(grid).Method("GetButtonPos", new object[] { element.gameObject }).GetValue<Vector2i>();
-            ItemDrop.ItemData itemData = inventory.GetItemAt(buttonPos.x, buttonPos.y);
-
-            if (itemData == null)
-                return;
-
-            if (Traverse.Create(InventoryGui.instance).Method("CanRepair", new object[] { itemData }).GetValue<bool>())
-            {
-                CraftingStation currentCraftingStation = Player.m_localPlayer.GetCurrentCraftingStation();
-                itemData.m_durability = itemData.GetMaxDurability();
-                if (currentCraftingStation)
-                {
-                    currentCraftingStation.m_repairItemDoneEffects.Create(currentCraftingStation.transform.position, Quaternion.identity, null, 1f);
-                }
-                Player.m_localPlayer.Message(MessageHud.MessageType.Center, Localization.instance.Localize("$msg_repaired", new string[]
-                {
-                            itemData.m_shared.m_name
-                }), 0, null);
-            }
-        }
-
-        //[HarmonyPatch(typeof(ItemDrop.ItemData), nameof(ItemDrop.ItemData.GetTooltip), new Type[] { typeof(ItemDrop.ItemData), typeof(int), typeof(bool) })]
-        public static class GetTooltip_Patch
-        {
-            public static void Postfix(ItemDrop.ItemData item, int qualityLevel, bool crafting, ref string __result)
-            {
-                if (modEnabled.Value && requireMats.Value && Player.m_localPlayer)
-                {
-
-                    List<ItemDrop.ItemData> wornItems = new List<ItemDrop.ItemData>();
-                    Player.m_localPlayer.GetInventory().GetWornItems(wornItems);
-
-                    if (wornItems.Find(i => i == item) == null)
-                        return;
-
-                    Recipe recipe = RepairRecipe(item);
-                    if (recipe == null)
-                    {
-                        __result += "\n" + freeTooltipString.Value;
-
-                    }
-                    else
-                    {
-                        bool playerEnough = true;
-                        List<string> reqstring = new List<string>();
-                        foreach (Piece.Requirement req in recipe.m_resources)
-                        {
-                            if (req.GetAmount(item.m_quality) == 0)
-                                continue;
-                            reqstring.Add($"{req.GetAmount(1)} {Localization.instance.Localize(req.m_resItem.m_itemData.m_shared.m_name)}");
-                            if (Player.m_localPlayer.GetInventory().CountItems(req.m_resItem.m_itemData.m_shared.m_name) < req.GetAmount(1))
-                                playerEnough = false;
-                        }
-
-
-                        if (Traverse.Create(Player.m_localPlayer).Method("HaveRequirements", new object[] { recipe.m_resources, false, item.m_quality }).GetValue<bool>())
-                        {
-                            if(playerEnough)
-                                __result += "\n" + string.Format(hasEnoughTooltipString.Value, string.Join(", ", reqstring));
-                            else
-                                __result += "\n" + string.Format(hasEnoughExternalTooltipString.Value, string.Join(", ", reqstring));
-                        }
-                        else
-                            __result += "\n" + string.Format(notEnoughTooltipString.Value, string.Join(", ", reqstring));
-                    }
-                }
-            }
-        }
-        
-        //[HarmonyPatch(typeof(InventoryGui), "CanRepair")]
-        public static class InventoryGui_CanRepair_Patch
-        {
-            public static void Postfix(ItemDrop.ItemData item, ref bool __result)
-            {
-                if (modEnabled.Value && requireMats.Value && Environment.StackTrace.Contains("RepairClickedItem") && !Environment.StackTrace.Contains("HaveRepairableItems") && __result == true && item?.m_shared != null && Player.m_localPlayer != null)
-                {
-                    Recipe recipe = RepairRecipe(item);
-                    if (recipe == null)
-                        return;
-
-                    List<string> reqstring = new List<string>();
-                    foreach (Piece.Requirement req in recipe.m_resources)
-                    {
-                        if (req?.m_resItem?.m_itemData?.m_shared == null)
-                            continue;
-                        reqstring.Add($"{req.GetAmount(item.m_quality)} {Localization.instance.Localize(req.m_resItem.m_itemData.m_shared.m_name)}");
-                    }
-                    string outstring;
-                    if (Traverse.Create(Player.m_localPlayer).Method("HaveRequirements", new object[] { recipe.m_resources, false, 1 }).GetValue<bool>())
-                    {
-                        Player.m_localPlayer.ConsumeResources(recipe.m_resources, item.m_quality);
-                        outstring = $"Used {string.Join(", ", reqstring)} to repair {Localization.instance.Localize(item.m_shared.m_name)}";
-                        __result = true;
-                    }
-                    else
-                    {
-                        outstring = $"Require {string.Join(", ", reqstring)} to repair {item.m_shared.m_name}";
-                        __result = false;
-                    }
-
-                    Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, outstring, 0, null);
-                    Dbgl(outstring);
-                }
-            }
-        }
-
-        public static Recipe RepairRecipe(ItemDrop.ItemData item)
-        {
-            float percent = (item.GetMaxDurability() - item.m_durability) / item.GetMaxDurability();
-            Recipe fullRecipe = ObjectDB.instance.GetRecipe(item);
-            var fullReqs = fullRecipe.m_resources.ToList();
-
-            bool isMagic = false;
-            if (epicLootAssembly != null)
-            {
-                isMagic = (bool)epicLootAssembly.GetType("EpicLoot.ItemDataExtensions").GetMethod("IsMagic", BindingFlags.Public | BindingFlags.Static).Invoke(null, new[] { item });
-            }
-            if (isMagic)
-            {
-                int rarity = (int)epicLootAssembly.GetType("EpicLoot.ItemDataExtensions").GetMethod("GetRarity", BindingFlags.Public | BindingFlags.Static).Invoke(null, new[] { item });
-                List<KeyValuePair<ItemDrop, int>> magicReqs =  (List<KeyValuePair<ItemDrop, int>>)epicLootAssembly.GetType("EpicLoot.Crafting.EnchantTabController").GetMethod("GetEnchantCosts", BindingFlags.Public | BindingFlags.Static).Invoke(null, new object[] { item, rarity });
-                foreach(var kvp in magicReqs)
-                {
-                    fullReqs.Add(new Piece.Requirement()
-                    {
-                        m_amount = kvp.Value,
-                        m_resItem = kvp.Key
-                    });
-                }
-            }
-
-            List<Piece.Requirement> reqs = new List<Piece.Requirement>();
-            Recipe recipe = ScriptableObject.CreateInstance<Recipe>();
-            for (int i = 0; i < fullReqs.Count; i++)
-            {
-
-                var req = new Piece.Requirement()
-                {
-                    m_resItem = fullReqs[i].m_resItem,
-                    m_amountPerLevel = Mathf.FloorToInt(fullReqs[i].m_amountPerLevel * percent * materialRequirementMult.Value),
-                    m_amount = Mathf.FloorToInt(fullReqs[i].m_amount * percent * materialRequirementMult.Value),
-                };
-
-                int amount = 0;
-                for (int j = item.m_quality; j > 0; j--)
-                {
-                    amount += req.GetAmount(j);
-                }
-
-
-                if (amount > 0)
-                {
-                    reqs.Add(req);
-                }
-            }
-            recipe.m_resources = reqs.ToArray();
-
-            if (!reqs.Any())
-            {
-                return null;
-            }
-            return recipe;
-        }
-
-
-        [HarmonyPatch(typeof(Terminal), "InputText")]
-        public static class InputText_Patch
-        {
-            public static bool Prefix(Terminal __instance)
+            static bool Prefix(Player __instance)
             {
                 if (!modEnabled.Value)
-                    return true;
-                string text = __instance.m_input.text;
-                if (text.ToLower().Equals("repairmod reset"))
                 {
-                    context.Config.Reload();
-                    context.Config.Save();
-                    Traverse.Create(__instance).Method("AddString", new object[] { text }).GetValue();
-                    Traverse.Create(__instance).Method("AddString", new object[] { "Repair Items config reloaded" }).GetValue();
+                    return true;
+                }
+
+                if (!__instance.InPlaceMode())
+                {
                     return false;
                 }
+
+                Piece hoveringPiece = __instance.GetHoveringPiece();
+                if (!hoveringPiece || !__instance.CheckCanRemovePiece(hoveringPiece) || !PrivateArea.CheckAccess(hoveringPiece.transform.position))
+                {
+                    return false;
+                }
+
+                WearNTear component = hoveringPiece.GetComponent<WearNTear>();
+                if (!component)
+                {
+                    return true;
+                }
+
+                float health = component.m_nview.GetZDO().GetFloat(ZDOVars.s_health, component.m_health);
+
+                if (health >= component.m_health)
+                {
+                    return true;
+                }
+
+                if (HaveEnemyInRange(__instance, component.m_nview.m_zdo.m_position, enemyBuildRange.Value))
+                {
+                    __instance.Message(MessageHud.MessageType.Center, "Es sind Gegner in der Nähe", 0, null);
+                    return false;
+                }
+
+                string outstring;
+                List<string> reqstring = new List<string>();
+                Recipe recipe = ScriptableObject.CreateInstance<Recipe>();
+                List<Piece.Requirement> reqs = new List<Piece.Requirement>();
+                float repairFactor = ((component.m_health - health) / component.m_health) * materialRequirementMult.Value;
+                bool retValue;
+
+                foreach (Piece.Requirement req in hoveringPiece.m_resources)
+                {
+                    Dbgl($"{req.m_resItem.m_itemData.m_shared.m_name} {req.GetAmount(1)}");
+                    int reqAmt = (int)Math.Ceiling(req.GetAmount(1) * repairFactor);
+                    reqstring.Add($"{reqAmt} {Localization.instance.Localize(req.m_resItem.m_itemData.m_shared.m_name)}");
+
+                    var rep_req = new Piece.Requirement()
+                    {
+                        m_resItem = req.m_resItem,
+                        m_amountPerLevel = 0,
+                        m_amount = reqAmt,
+                    };
+
+                    reqs.Add(rep_req);
+                }
+
+                recipe.m_resources = reqs.ToArray();
+                recipe.m_item = null;
+                recipe.m_craftingStation = null;
+                recipe.m_repairStation = null;
+                recipe.m_minStationLevel = 0;
+
+                if (__instance.HaveRequirementItems(recipe, false, 1))
+                {
+                    __instance.ConsumeResources(recipe.m_resources, 1);
+                    outstring = $"{string.Join(", ", reqstring)} zur Reparatur von {Localization.instance.Localize(hoveringPiece.m_name)} verwendet";
+                    retValue = true;
+                }
+                else
+                {
+                    outstring = $"{string.Join(", ", reqstring)} zur Reparatur von {Localization.instance.Localize(hoveringPiece.m_name)} erforderlich";
+                    retValue = false;
+                }
+
+                __instance.Message(MessageHud.MessageType.TopLeft, outstring, 0, null);
+                Dbgl(outstring);
+                return retValue;
+            }
+        }
+
+        [HarmonyPatch(typeof(Player), nameof(Player.TryPlacePiece))]
+        static class Player_TryPlacePiece_Patch
+        {
+            static bool Prefix(Player __instance, Piece piece)
+            {
+                if (piece.m_resources.Length > 0)
+                {
+                    if (HaveEnemyInRange(__instance, __instance.m_nview.m_zdo.m_position, enemyBuildRange.Value))
+                    {
+                        __instance.Message(MessageHud.MessageType.Center, "Es sind Gegner in der Nähe", 0, null);
+                        return false;
+                    }
+                }
+
                 return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(Player), nameof(Player.CheckCanRemovePiece))]
+        static class Player_RemovePiece_Patch
+        {
+            static void Postfix(Player __instance, ref bool __result, Piece piece)
+            {
+                if (Environment.StackTrace.Contains("Player.RemovePiece") && (__result == true))
+                {
+                    WearNTear component = piece.GetComponent<WearNTear>();
+                    if (!component)
+                    {
+                        return;
+                    }
+
+                    if (component.m_nview.GetZDO().GetFloat(ZDOVars.s_health, component.m_health) < component.m_health)
+                    {
+                        __result = false;
+                        __instance.Message(MessageHud.MessageType.Center, "Beschädigtes Objekt kann nicht abgerissen werden", 0, null);
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(WearNTear), nameof(WearNTear.Destroy))]
+        static class WearNTear_Destroy_Patch
+        {
+            static void Prefix(ref bool blockDrop)
+            {
+                if (Environment.StackTrace.Contains("ApplyDamage"))
+                {
+                    blockDrop = true;
+                }
             }
         }
     }
